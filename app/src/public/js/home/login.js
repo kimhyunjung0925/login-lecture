32151478b8641c3504037f1acfc493dd30cdd20b
apf_temp_admin <khjung0925@naver.com>
2024-01-09 01:24:54 +0900

    돔을 이용하여 HTML 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,16 @@
 "use strict"
 
-console.log("by")
+const id = document.querySelector("#id"), 
+    psword = document.querySelector("#psword"),
+    loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click",login)
+
+function login() {
+    console.log(id.value);
+    const req = {
+        id : id.value,
+        password : password.value,
+    };
+    console.log(req);
+}
