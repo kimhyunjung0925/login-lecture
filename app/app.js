50ec4b65be9d37e2ffe8de1d8cc6d74dd7a68ceb
apf_temp_admin <khjung0925@naver.com>
2024-01-09 00:41:21 +0900

    폴더구조 최적화

--- app/app.js
+++ app/app.js
@@ -5,13 +5,13 @@ const express = require('express');
 const app = express();
 
 //앱세팅
-app.set("views","./views");
+app.set("views","./src/views");
 app.engine('html', require('ejs').renderFile);
 app.set("view engine","html");
 
 
 //라우팅
-const home = require("./routes/home");
+const home = require("./src/routes/home");
 app.use("/", home); //=> 미들웨어 등록해주는 메서드
 
 module.exports = app;
